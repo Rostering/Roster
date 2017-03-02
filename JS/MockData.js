f4029920116967b895cc5caa94299d097276cd6a
AllanLiang <luliangallan@gmail.com>
2017-03-02 21:17:48 +1300

    Prepare the mock data of sites and staffs

---
+++ JS/MockData.js
@@ -0,0 +1,125 @@
+var Sites = [
+  {
+    id:1,
+    name:"Site A",
+  },
+  {
+    id:2,
+    name:"Site B",
+  },
+  {
+    id:3,
+    name:"Site C",
+  },
+  {
+    id:4,
+    name:"Site D",
+  },
+  {
+    id:5,
+    name:"Site E",
+  },
+];
+
+var Staffs = [
+  {
+    id:1,
+    name:"Allan",
+    siteId: 1,
+  },
+  {
+    id:2,
+    name:"Krzysiek",
+    siteId: 1,
+  },
+  {
+    id:3,
+    name:"Daphne",
+    siteId: 1,
+  },
+  {
+    id:4,
+    name:"Yesfir",
+    siteId: 1,
+  },
+  {
+    id:5,
+    name:"Brenton",
+    siteId: 2,
+  },
+  {
+    id:6,
+    name:"Francisco",
+    siteId: 2,
+  },
+  {
+    id:7,
+    name:"Suellen",
+    siteId: 3,
+  },
+  {
+    id:8,
+    name:"Wen",
+    siteId: 3,
+  },
+  {
+    id:9,
+    name:"Puanani",
+    siteId: 3,
+  },
+  {
+    id:10,
+    name:"Milon",
+    siteId: 4,
+  },
+  {
+    id:11,
+    name:"Bedelia",
+    siteId: 4,
+  },
+  {
+    id:12,
+    name:"Meridian",
+    siteId: 4,
+  },
+  {
+    id:13,
+    name:"Nancy",
+    siteId: 4,
+  },
+  {
+    id:14,
+    name:"April",
+    siteId: 4,
+  },
+  {
+    id:15,
+    name:"Caetano",
+    siteId: 5,
+  },
+  {
+    id:16,
+    name:"Due",
+    siteId: 5,
+  },
+  {
+    id:17,
+    name:"Gavin",
+    siteId: 5,
+  },
+  {
+    id:18,
+    name:"Aeron",
+    siteId: 2,
+  },
+  {
+    id:19,
+    name:"Jayron",
+    siteId: 5,
+  },
+  {
+    id:20,
+    name:"Kizzie",
+    siteId: 3,
+  },
+];

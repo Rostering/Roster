a78b92f26fe6418526938560415f75694a280459
AllanLiang <luliangallan@gmail.com>
2017-03-03 00:30:07 +1300

    Use Kendo grid and template to render

--- JS/Index.js
+++ JS/Index.js
@@ -12,4 +12,20 @@ $(function(){
   });
   $("#startTime").kendoTimePicker();
   $("#finishTime").kendoTimePicker();
+
+  $("#scheduler").kendoGrid({
+    height: 500,
+    rowTemplate: kendo.template($("#template").html()),
+    columns: [
+        { title: "Staff" },
+        { title: "Mon" },
+        { title: "Tus" },
+        { title: "Wed" },
+        { title: "Thu" },
+        { title: "Fri" },
+        { title: "Sat" },
+        { title: "Sun" },
+    ],
+    dataSource: Staffs,
+  });
 })
